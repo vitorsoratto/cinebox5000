7a18298a928ff09b585ce9a1f8155cda2837c839
Vitor Soratto <sorattoemail@gmail.com>
2025-08-14 10:27:54 -0300

    feat: Splash screen layout to use a stack for background and logo

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,7 +1,6 @@
-import 'package:cinebox5000/config/env.dart';
+import 'package:cinebox5000/ui/core/themes/resource.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:cinebox5000/ui/core/themes/resource.dart';
 
 class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
@@ -14,13 +13,24 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Image.asset(R.ASSETS_IMAGES_BG_LOGIN_PNG),
-            Text(Env.backendBaseUrl),
-          ],
-        ),
+      body: Stack(
+        children: [
+          Image.asset(
+            R.ASSETS_IMAGES_BG_LOGIN_PNG,
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            constraints: const BoxConstraints.expand(),
+            decoration: BoxDecoration(
+              color: Colors.black54,
+            ),
+          ),
+          Center(
+            child: Image.asset(R.ASSETS_IMAGES_LOGO_PNG),
+          ),
+        ],
       ),
     );
   }
